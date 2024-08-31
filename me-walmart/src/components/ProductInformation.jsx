6b6572b434e0a3441892e3c946beb1709219c0e0
Joseph Esposito <josephesposito0424@gmail.com>
2024-08-31 11:54:24 -0400

    Styling of product page, the product info component to align it closer with mobile design.

--- me-walmart/src/components/ProductInformation.jsx
+++ me-walmart/src/components/ProductInformation.jsx
@@ -3,6 +3,7 @@ import NotFound from './NotFound';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+
 function ProductInformation(){
     const { productId } = useParams();
     const [error, setError] = useState(true);
@@ -36,31 +37,51 @@ function ProductInformation(){
         <div className="product-container">
             <img className="product-image-main" src="/BLACK-DECKER-6-Slice-Crisp-N-Bake-Air-Fry-Toaster-Oven-TO3217SS.webp" alt="Temporary product image."></img>
             <div className="product-information-container">
-                <span className="product-name">{product.full_name}</span>
-                <div className="product-price-container">
-                    <span className="product-price">{USDollar.format(product.price)}/ea</span>
-                </div>
-                <span>Item # {product.item_number}</span>
-                <span className="product-upc">UPC {productId}</span>
-                <hr></hr>
-                <div className="product-details-three">
-                    <div className="product-size-container">
-                        <span>Size</span>
-                        <span>{product.size}</span>
+                <div className="product-card-main">
+                    <div className="product-card-top">
+                        <span className="product-name">{product.full_name}</span>
+                        <div className="product-price-container">
+                            <span className="product-price">{USDollar.format(product.price)}/ea</span>
+                        </div>
+                        <div className = "product-number-container">
+                            <span>Item #</span>
+                            <span className="product-value">{product.item_number}</span>
+                        </div>
+                        <div className = "product-department-container">
+                            <span>Department</span>
+                            <span className="product-value">{product.department}</span>
+                        </div>
+                        <div className="product-category-container">
+                            <span>Category</span>
+                            <span className="product-value">{product.category}</span>
+                        </div>
+                        <div className="product-upc-container">
+                            <span>UPC</span>
+                            <div>
+                                <span className="product-value">{product.upc.substring(0, product.upc.length - 4)}</span>
+                                <span id="product-upc-last-four" className="product-value">{product.upc.substring(product.upc.length - 4)}</span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="product-details-border"></div>
-                    <div className="product-color">
-                        <span>Color</span>
-                        <span>{product.color}</span>
+                    <hr></hr>
+                    <div className="product-details-three">
+                        <div className="product-size-container">
+                            <span>Size</span>
+                            <span>{product.size}</span>
+                        </div>
+                        <div className="product-details-border"></div>
+                        <div className="product-color">
+                            <span>Color</span>
+                            <span>{product.color}</span>
+                        </div>
+                        <div className="product-details-border"></div>
+                        <div className="product-case-pack">
+                            <span>Case pack</span>
+                            <span>{product.case_pack} ea</span>
+                        </div>
                     </div>
-                    <div className="product-details-border"></div>
-                    <div className="product-case-pack">
-                        <span>Case pack</span>
-                        <span>{product.case_pack} ea</span>
-                    </div>
-                </div>
-                <hr></hr>
-                <div id="pd-bottom" className="product-details-three">
+                    <hr></hr>
+                    <div id="pd-bottom" className="product-details-three">
                     <div className="product-on-hand-count">
                         <span>On hand</span>
                         <span>{product.on_hand_count}</span>
@@ -77,6 +98,7 @@ function ProductInformation(){
                     </div>
                 </div>
                 <hr></hr>
+                </div>
             </div>
         </div>
         );
